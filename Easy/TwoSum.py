5e5ab12cda51ad9c33914d9ad254f248091e9330
HaiderMalikk <haider.malik@student.tdsb.on.ca>
2024-01-22 23:13:08 -0500

    first commit

---
+++ Easy/TwoSum.py
@@ -0,0 +1,17 @@
+Array = [1 ,3 , 5, 9, 11]
+Target = 8 # 3 + 5
+
+class Solution:
+    def TwoSum(array, target):
+        n = len(array)
+        for i in range(0, n - 1):
+            for j in range(i + 1, n):
+                if array[i] + array[j] == target:
+                    return f'Index numbers: [{i} {j}],  Index Values: [{array[i]} {array[j]}]'
+        return ("no two sum")
+        
+
+print(Solution.TwoSum(Array, Target))
+    
+    
+    
