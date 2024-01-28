14d840cf3d98c6d573971cc1650037feefff9d6a
Haider Malik <haidermalik662@gmail.com>
2024-01-28 00:09:05 -0500

    comments

--- Easy/removeElement.py
+++ Easy/removeElement.py
@@ -1,12 +1,12 @@
 def removeElement(nums, val):
         index = 0
 
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[index] = nums[i]
-                index += 1
+        for i in range(len(nums)): # loop over all elements in nums
+            if nums[i] != val: # if the element of nums at i is not equal to val
+                nums[index] = nums[i] # take the index value and put i on its place
+                index += 1 # increment the index as we now have a occurance of val
 
-        return index
+        return index # return index the occurance of val in nums
 
-solution = removeElement([0,1,2,2,3,0,4,2], 2)
+solution = removeElement([0,1,2,2,3,0,4,2], 2) # list, target
 print(solution)
