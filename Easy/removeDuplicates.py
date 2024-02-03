0d9a2ca0bf83341f7fdc6f21930e237f1e6b0ba4
Haider Malik <haidermalik662@gmail.com>
2024-02-02 23:53:24 -0500

    comments

--- Easy/removeDuplicates.py
+++ Easy/removeDuplicates.py
@@ -1,10 +1,10 @@
 def removeDuplicates(nums): # dynamicly changing len of array
-    i = 0
-    while i < len(nums) - 1:
-        if nums[i] == nums[i + 1]:
-            nums.pop(i + 1)
+    i = 0 # start a counter
+    while i < len(nums) - 1: # kinda a for loop runs until we have checked all elements in the list.
+        if nums[i] == nums[i + 1]:#  check to see if current element is equal to next one if so then it's a duplicate
+            nums.pop(i + 1) # remove duplicate NOTE pop(index) while remove(element) 
         else:
-            i += 1
+            i += 1 # if there is no duplicates move on to next element but we treversed 1 element to increment counter by 1 to keep track of list
     return nums
 
 nums = [1, 1, 1, 2, 3, 3, 4]
