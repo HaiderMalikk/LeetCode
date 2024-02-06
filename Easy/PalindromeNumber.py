11ccba0c56686b0e425b0c44f22e37a1629b0930
Haider Malik <haidermalik662@gmail.com>
2024-02-05 22:57:46 -0500

    comments

--- Easy/PalindromeNumber.py
+++ Easy/PalindromeNumber.py
@@ -1,12 +1,12 @@
-def PalindromeNumber(x: int):
-    numStr = str(x)
-    if numStr == numStr[::-1]:
-        return True
+def PalindromeNumber(x: int): # EZ METHOD
+    numStr = str(x) # convert to string  for easy reversal
+    if numStr == numStr[::-1]: # ::-1 reverses the array to characters the string
+        return True # if palindrome meaning same number forward and backward, return true
     else:
-        return False
+        return False # else false
 
-var = PalindromeNumber(121)
-var2 = PalindromeNumber(12234)
+var = PalindromeNumber(121) # true 121 = 121 
+var2 = PalindromeNumber(12234) # false 12234 = 43221
 print(var)
 print(var2)
 
