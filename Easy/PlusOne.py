5fe602f11d1b3227d42866c2c80810a8f4ae1f27
Haider Malik <haidermalik662@gmail.com>
2024-02-09 23:54:11 -0500

    fix

--- Easy/PlusOne.py
+++ Easy/PlusOne.py
@@ -1,9 +1,11 @@
-def plusOne(digits):
-    if digits[0] == 9:
-        digits[0] = 1
-        digits.append(0)
-    else:
-        digits[-1] = digits[-1] + 1
-    return digits
+# def plusOne(digits):
+#     if digits[0] == 9:
+#         digits[0] = 1
+#         digits.append(0)
+#     else:
+#         digits[-1] = digits[-1] + 1
+#     return digits
 
-print(plusOne([9]))
+# print(plusOne([9]))
+
+# NOTE yea i kinda messed this up if you have 19 or 99 it messes it up TODO 
