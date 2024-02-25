9830b2c29ad2f5f74ea0397a1c97efd951b0f303
HaiderMalikk <haidermalik662@gmail.com>
2024-02-24 23:43:21 -0500

    new: panagram, updated: reverseStr

--- Easy/reverseString.py
+++ Easy/reverseString.py
@@ -1,15 +1,14 @@
-class Solution(object):
-    def reverseString(self, s):
-        # Initialize two pointers, one at the beginning and one at the end of the list
-        start, end = 0, len(s) - 1
+def RevStr(s: list) -> list:
+    left = 0 # left starts at start of array index 0
+    right = len(s)-1  # right is set to the last element in the array, the -1 is there as we are using zero based indexing (index starts at zero).
+    while left < right: # left ++ and right -- insure the pointers will meet in the middel where to flip in needed
+        temp = s[left] # temporarily store the value of the left element
+        s[left] = s[right] # in the first iteration let the first element be the last one then the second one be the second lsat one etc
+        s[right] = temp # let the right element be the left one this is s[right] = s[left] but in theline above we gave s[left] a value so to remember left we set temp = s[left]
+        left += 1 # left pointer moves up one element (forwards)
+        right -= 1# right pointer moves down a element (backwards)
+        # NOTE we could have done # s[left], s[right] = s[right], s[left]  but that would not be efficient
 
-#        Iterate until the pointers meet in the middle
-        while start < end:
-            s[start], s[end] = s[end], s[start]
-            # Move the pointers towards each other
-            start += 1
-            end -= 1
+    return s # once done return new str
 
-# The list has been modified in-place, and there's no need to return anything
-#NOTE: this is a leetcode solution will not run in vs code etv
-
+print(RevStr(["a","b","c","d","e"])) # ["e", "d", "c" , "b" , "a"]
